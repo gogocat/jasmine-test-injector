fef318e090e589739a6d4c77103eb7c174e02e89
gogocat <adamchow2326@yahoo.com.au>
2015-03-19 23:45:59 +1100

    updated test

---
+++ test/spec/jasmine-test-injector-spec.js
@@ -0,0 +1,14 @@
+
+// start jasmine unit testing
+describe("jasmine-test-injector", function() {
+	
+
+	it("$r should be a global function", function() {
+		expect(typeof $r).toBe("function");
+	});
+	
+	it("$r should call through require", function() {
+		
+	});
+
+});
