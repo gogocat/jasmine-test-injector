d3e480e5f24c8f5b6f6dd1a15186db904ebeb058
gogocat <adamchow2326@yahoo.com.au>
2015-03-20 20:28:39 +1100

    updated test

--- test/spec/jasmine-test-injector-spec.js
+++ test/spec/jasmine-test-injector-spec.js
@@ -3,11 +3,15 @@
 describe("jasmine-test-injector", function() {
 	
 
-	it("$r should be a global function", function() {
+	it("$inject should be a global function", function() {
 		expect(typeof $r).toBe("function");
 	});
 	
-	it("$r should call through require", function() {
+	it("$inject should call through", function() {
+		
+	});
+	
+	it("function a inside closure should call through", function() {
 		
 	});
 
