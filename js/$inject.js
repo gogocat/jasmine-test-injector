502b60d9651e1930b5ad4f71ba36bccf38a4dc82
gogocat <adamchow2326@yahoo.com.au>
2015-03-27 22:12:38 +1100

    added

---
+++ js/$inject.js
@@ -0,0 +1,85 @@
+/*
+*	$inject.js
+*	Inject script into closure
+*/
+(function($) {
+"use strict";
+var cache = {};
+
+function Inject(){
+} 
+
+function loader(uri, callback) {
+	var isAsync = (typeof callback === "function"),
+		request,
+		wrapScript,
+		ret;
+	
+	if (typeof uri !== "string") {
+		throw  "script url is undefined";
+	}
+	
+	if (cache[uri]) {
+		return callback(cache[uri]);
+	}
+	
+	wrapScript = function(responseText, dataType) {
+		var closureFn,
+			closureFnText,
+			source;
+			
+		// TODO: seal closure
+		if (responseText) {	
+			closureFnText = '"use strict";\n var module = {}, exports = {}; \n';
+			closureFnText += responseText;
+			closureFnText += '\n //# sourceURL='+ uri;
+			closureFn = new Function(closureFnText);
+			cache[uri] = source = closureFn(); // Make the closureFn
+			return source;
+		} 
+		return null;
+	};
+
+	request = $.ajax({
+		url: uri,
+		type: 'GET',
+		dataType: "script",
+		async: isAsync,
+		cache:true,
+		crossDomain: false,
+		dataFilter: wrapScript,
+		success: function(closureFn) {
+			if(!closureFn) {
+				return;
+			}
+			if (isAsync) {
+				callback(closureFn);
+			} else {
+				ret = closureFn;
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			throw errorThrown;
+		}
+	});
+	
+	return (isAsync) ? request : ret;
+}
+	
+ 
+// convert function body to string
+function getFnBodyString(fn) {
+    var fnString,
+        fnBody;
+    
+    if (typeof fn !== "function") {
+        return;
+    }
+    fnString = fn.toString();
+    fnBody = fnString.substring(fnString.indexOf("{") + 1, fnString.lastIndexOf("}"));
+    return fnBody;
+}
+
+
+
+}(jQuery));
