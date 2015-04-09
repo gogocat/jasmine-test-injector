d39c92aa126b7a8a80a508b0476ca986e5d1087e
gogocat <adamchow2326@yahoo.com.au>
2015-04-10 01:33:17 +1000

    WIP

--- test/spec/jasmine-test-injector-spec.js
+++ test/spec/jasmine-test-injector-spec.js
@@ -1,22 +1,11 @@
 
 // start jasmine unit testing
-/*
+
 describe("jasmine-test-injector", function() {
 	"use strict";
-	
-	var value = 0;
-	
-	beforeEach(function(done) {
+
+	beforeEach(function() {
 		spyOn(window, "$inject");
-		 setTimeout(function() {
-		  value += 1;
-		  done();
-		}, 1);
-	});
-	
-	it("Asyn test value should greater than 0", function(done) {
-		expect(value).toBeGreaterThan(0);
-		done();
 	});
 	
 	it("$inject should be a global function", function() {
@@ -30,27 +19,32 @@ describe("jasmine-test-injector", function() {
 	
 });
 
-*/
 
 $inject("http://localhost/jasmine-test-injector/js/testiife.js", function() {
 	
 	describe("Test $injector into IIFE script", function() {
+		
 		beforeEach(function(done) {
 			setTimeout(function() {
 				done();
 			}, 1);
 		});
+		
 		it("variable 'name' inside loaded IIFE should be IIFE!", function(done) {
 			expect(name).toBe("IIFE!");
 			done();
 		});
+		
+		it("call function add(2,2) should return 4", function(done) {
+			var result = add(2,2);
+			expect(result).toBe(4);
+			done();
+		});
+		
 		it("window.IIFE.sum should be 2", function(done) {
 			expect(window.IIFE.sum).toBe(2);
 			done();
 		});
-		//it("testModule variable 'testModuleName' inside closure should be accessible", function() {
-			//expect(typeof testModuleName).toBe("undefined");
-		//});
 	});
 	//# sourceURL=testiife.js;
 });
